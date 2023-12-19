dbb7bdc2916c2f4b48e9230c2a085f037eee8d18
absence <usturakeserway@gmail.com>
2023-12-19 23:58:45 +0300

    signals
    working on signal system

---
+++ abscore/Assets/_Scripts/Systems/SignalSystem/SignalGenerator.cs
@@ -0,0 +1,43 @@
+using com.absence.core.systems.signals;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.absence.core.systems.signals
+{
+    public class SignalGenerator : MonoBehaviour
+    {
+        // Fields.
+        [SerializeField] private bool m_currentState = true;
+
+        // Private.
+        private SignalGate m_controller;
+
+        // Properties.
+        public Action OnSignalChanged;
+        public bool SignalState => m_currentState;
+
+        private void Start()
+        {
+            if (m_controller == null) this.enabled = false;
+        }
+
+        public void SetSignal(bool newState)
+        {
+            if (m_currentState == newState) return;
+            NegateSignal();
+        }
+
+        public void NegateSignal()
+        {
+            m_currentState = !m_currentState;
+            OnSignalChanged?.Invoke();
+        }
+        public void SetController(SignalGate controller)
+        {
+            m_controller = controller;
+        }
+    }
+
+}
---
+++ abscore/Assets/_Scripts/Systems/SignalSystem/SignalListener.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.absence.core.systems.signals
+{
+    public class SignalListener : MonoBehaviour
+    {
+        // Fields.
+        [SerializeField] private bool m_negateSignals = false;
+
+        // Private.
+        private SignalGate m_controller;
+        private bool m_lastSignalState;
+
+        // Properties.
+        public event Action OnPositiveSignalReceived;
+        public event Action OnNegativeSignalReceived;
+        public event Action OnSignalChanged;
+        public event Action OnSignalChangedToPositive;
+        public event Action OnSignalChangedToNegative;
+        public bool NegateIncomingSignals => m_negateSignals;
+
+        private void Start()
+        {
+            if (m_controller == null) this.enabled = false;
+        }
+
+        public void ListenToSignal(bool signalState)
+        {
+            if (m_negateSignals) signalState = !signalState;
+
+            if (signalState) OnPositiveSignalReceived?.Invoke();
+            else OnNegativeSignalReceived?.Invoke();
+            if (m_lastSignalState != signalState)
+            {
+                OnSignalChanged?.Invoke();
+                if (signalState) OnSignalChangedToPositive?.Invoke();
+                else OnSignalChangedToNegative?.Invoke();
+            }
+
+            m_lastSignalState = signalState;
+        }
+
+        public void SetController(SignalGate controller)
+        {
+            m_controller = controller;
+        }
+    }
+
+}
